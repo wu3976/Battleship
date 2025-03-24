a8045f243a5a4b6d55806ac9d98b85ceec399829
吴辰杰 <wuchenjie@wuchenjiedeMacBook-Air.local>
2025-03-24 02:16:03 -0700

    v1.4: modified styles

--- src/components/BoardCell.tsx
+++ src/components/BoardCell.tsx
@@ -23,8 +23,7 @@ export default function BoardCell({ cellState, showShip, cellId } : BoardCellPro
         bgColor = !showShip && hovered ? BGLIGHTBLUE_HOVER :BGLIGHTBLUE;
         color = BGLIGHTBLUE;
     } else if (cellState === 1) {
-        // cellContent = showShip ? "⚫" : "";
-        cellContent = "⚫" ;
+        cellContent = showShip ? "⚫" : "";
         bgColor = !showShip && hovered ? BGLIGHTBLUE_HOVER : BGLIGHTBLUE;
         color = BLACK;
     } else if (cellState === 2) {
